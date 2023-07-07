5525da1bc261d5b4a213c33bbe71b57fe20e2c31
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-07 22:53:28 +0530

    temprature.c

---
+++ temprature.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+
+    float temprature=0;
+
+    printf("enter the value of temprature");
+    scanf("%f",&temprature);
+
+    // check
+
+    if(temprature>100)
+    {
+
+        printf("temprature");
+
+    }
+    printf("\ngoodbyeee...");
+}
