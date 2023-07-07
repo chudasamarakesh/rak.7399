fd5d8d82bc9d1e60e3aa261015457d8dcc5d7f3e
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-07 23:10:03 +0530

    alphabet2.c

---
+++ alphabet2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char letter=0;
+
+printf("enter the value of letter");
+scanf("%c",&letter);
+// check
+if(letter<'a')
+{
+
+    printf("a is greater");
+
+}
+printf("\ngood byeee....");
+
+}
