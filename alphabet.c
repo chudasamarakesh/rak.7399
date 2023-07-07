ef630199653da1066ba0eec77b3f6a946f0aaeb1
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-07 22:39:32 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+
+char alphabet='0';
+
+printf("enter the value of alphabet");
+scanf("%c",&alphabet);
+
+// check
+
+if(alphabet !='a')
+{
+printf("a is greater");
+
+
+
+}
+printf("\ngoodbyeee");
+}
