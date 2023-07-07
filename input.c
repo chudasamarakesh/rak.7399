bd1ee36b99061718008ff4f029a04e98ad010553
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-07 20:18:40 +0530

    rak4.c

--- input.c
+++ input.c
@@ -5,5 +5,5 @@ void main ()
     int number = 45;
     int num= 65;
 
-    printf("the value of a is %d and the value of b is  %d",number,num);
+    printf("the value of a is %d and the value of b is  %d",number,num) ;
 }
