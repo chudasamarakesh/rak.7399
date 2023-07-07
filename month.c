0bae293dc95b67139c8e1553a9163a24fc47ea3b
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-07 22:24:44 +0530

    month.c

---
+++ month.c
@@ -0,0 +1,18 @@
+// write a programme to check if the month
+#include<stdio.h>
+void main()
+
+
+{
+int month=0;
+
+printf("enter the value of month");
+scanf("%d",&month);
+// check
+if(month==2)
+{
+    printf("this month has 28-29 days");
+
+}
+printf("\ngoodbyeee....");
+}
