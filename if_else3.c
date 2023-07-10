9d4a1fe981b6f49f7c0c1ee5cd198079c2de0a44
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-10 17:24:38 +0530

    if_else3.c

---
+++ if_else3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+
+    int year=0;
+
+    printf("enter the value of year");
+    scanf("%d",&year);
+
+    // check
+
+
+    if(year% 2==0)
+    {
+        printf("even");
+
+    }
+    else
+    {
+
+        printf("odd");
+    }
+
+
+
+}
