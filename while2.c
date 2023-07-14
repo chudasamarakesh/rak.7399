c1374e14a326d566f12901c43f872fdabf2b8182
chudasamarakesh <rakeshchudasama7399@gmail.com>
2023-07-14 22:09:20 +0530

    while2.c

---
+++ while2.c
@@ -0,0 +1,16 @@
+// write a programe to print 1 to 5000 number
+#include<stdio.h>
+void main()
+{
+
+    int number=1;
+
+    while(number<5000)
+    {
+
+        printf("%d",number);
+        number=number+1;
+        // printf("%d",number);
+        // number=number+1;
+    }
+}
